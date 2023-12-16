025222627f06b24ec82274d29aa5953187dd677f
Nathan <nathan19@umd.edu>
2023-12-16 18:39:27 -0500

    login, signup, deletetasks

--- ServerStaticFiles/js/todo.js
+++ ServerStaticFiles/js/todo.js
@@ -1,12 +1,32 @@
 const addButton = document.getElementById("add-task");
 const taskContainer = document.getElementById("task-area");
+const date = document.getElementById("todo-date");
 
-
-const url = `mongodb+srv://tgacquin:<password>@cluster0.4zzdwnk.mongodb.net/?retryWrites=true&w=majority`
-
+let taskArray = [];
 let taskId = 0; // Initialize a variable to keep track of task IDs
 
 addButton.addEventListener("click", addTask);
+date.addEventListener('change', dateSelect);
+
+async function dateSelect() {
+    const dateValue = date.value;
+    const response = await fetch("/todo", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ date: dateValue })
+    });
+    const result = await response.json();
+    console.log(result);
+}
+
+function renderTasks() {
+    taskContainer.innerHTML = "";
+    taskArray.forEach(element => {
+        taskContainer.appendChild(element);
+    });
+}
 
 function addTask() {
     console.log("Adding new Task");
@@ -45,33 +65,24 @@ function addTask() {
     let deleteButton = document.createElement("ion-icon");
     deleteButton.className = "delete-button";
     deleteButton.name = "trash-outline";
-    deleteButton.onclick = () => {
+    deleteButton.onclick = (event) => {
+        // Retrieve the parent element of the delete button
+        let taskElement = event.target.parentElement;
+        console.log(taskElement);
+
+        // Remove the task from the DOM
+        let indexToRmv = taskArray.indexOf(taskElement);
+        taskArray.splice(indexToRmv,1);
+        renderTasks();
+        // Send a request to delete the task from the backend
         console.log("DELETE DAT HOE");
     };
     newTaskDiv.appendChild(deleteButton);
 
+    taskArray.push(newTaskDiv);
 
     //Adding the new task to the task-area div
     taskContainer.appendChild(newTaskDiv);
     taskText.value = "";
     taskId++; // Increment the task ID for the next task
 }
-
-
-document.getElementById('login-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-    })
-    .then(response => response.json())
-    .then(data => console.log('Login successful:', data))
-    .catch(error => console.error('Error:', error));
-});
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const portNumber = 5001;
 const express = require('express');
-const { MongoClient } = require('mongodb');
-require("dotenv").config();
 const app = express();
 const path = require('path');
 const bodyParser = require("body-parser"); /* To handle post parameters */
 
+/* Setup for mongoDB */
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') }) 
+ /* Our database and collection */
+const { MongoClient} = require('mongodb');
+const { request } = require('http');
+const uri = process.env.MONGO_CONNECTION_STRING;
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+const client = new MongoClient(uri); 
+connectToDB();
+
+const session = require('express-session');
+
+app.use(session({
+    // Session configuration
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 /* Initializes request.body with post information */ 
 app.use(bodyParser.urlencoded({extended:false}));
-
 
 /* Setting path to use ejs templates*/
 app.set("views", path.resolve(__dirname, "templates"));
@@ -20,8 +36,61 @@ app.use('/serverStaticFiles', express.static(publicPath));
 
 app.use(express.json());
 
+/* Server and Mongo Connection */
+const server = app.listen(portNumber, () => {
+    console.log(`Web server is running at http://localhost:${portNumber}`);
+}); 
+
+async function connectToDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 app.get("/", (request, response) => {
-    response.render("index");
+    response.render("index", {port: portNumber, userNotFound: false});
+});
+
+app.get("/signUp", (request, response) => {
+    response.render("signUp", {port: portNumber, passwordConfirmed: true});
+});
+
+app.post("/signUp", async (request, response) => {
+    try {
+        let {signUpUsername, signUpPassword, confirmPassword} = request.body;
+        if (signUpPassword !== confirmPassword) {
+            response.render("signUp", {port: portNumber, passwordConfirmed: false});
+            return;
+        }
+        let newUser = {username: signUpUsername, password: signUpPassword};
+        const result = await client.db(databaseAndCollection.db)
+        .collection(databaseAndCollection.collection)
+        .insertOne(newUser);
+        response.render("index", {port: portNumber, userNotFound: false});
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+});
+
+app.post("/login", async (request, response) => {
+    
+    console.log("poop");
+    let {username, password} = request.body;
+    let userInfo = {username, password};
+    const result = await client.db(databaseAndCollection.db)
+    .collection(databaseAndCollection.collection)
+    .findOne(userInfo);
+    console.log(result);
+    console.log(databaseAndCollection.db);
+    if (result !== null) {
+        response.render("options");
+        request.session.username = result.username;
+    } else {
+        response.render("index", {port: portNumber, userNotFound: true });
+    }
 });
 
 app.get("/get-quote", async (request, response) => {
@@ -35,26 +104,47 @@ app.get("/get-quote", async (request, response) => {
     }
 });
 
-const server = app.listen(portNumber, () => {
-    console.log(`Web server is running at http://localhost:${portNumber}`);
-}); 
+app.get("/todo", async (request, response) => {
+    response.render("todo");
+});
 
-const uri = "mongodb+srv://tgacquin:iriZjkp5YV7zq3P9@cluster0.4zzdwnk.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-const database = client.db("YourDatabaseName");
-const usersCollection = database.collection("users");
+app.post("/todo", async (request, response) => {
+    if (!request.session.username) {
+        return response.status(401).send('User not logged in');
+    }
 
-app.post('/login', async (req, res) => {
+    const username = request.session.username;
+    const selectedDate = request.body.date;
+    
+    const userTodos = await client.db(databaseAndCollection.db)
+                                  .collection(databaseAndCollection.collection)
+                                  .findOne({ username: username, 'todos.date': selectedDate });
+
+    if (userTodos) {
+        const todoList = userTodos.todos.find(todo => todo.date === selectedDate);
+        response.send({ todoList: todoList });
+    } else {
+        response.send({ todoList: [] });
+    }
+});
+
+app.delete('/delete-task/:taskId', async (req, res) => {
+    const taskId = req.params.taskId;
+    // Assuming 'username' is stored in the session and each task is tied to a username
+    const username = req.session.username;
+
+    // Delete the task from the database
+    // The actual implementation depends on your database and schema
     try {
-        await client.connect();
-        const { username, password } = req.body;
-        // Here you can add code to hash the password before storing it
-        const result = await usersCollection.insertOne({ username, password });
-        res.status(200).json({ message: "User added", userId: result.insertedId });
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ message: "Error processing request" });
-    } finally {
-        await client.close();
+        await client.db(databaseAndCollection.db)
+                    .collection(databaseAndCollection.collection)
+                    .updateOne(
+                        { username: username },
+                        { $pull: { tasks: { id: taskId } } }  // Assuming each task has an 'id' field
+                    );
+        res.status(200).send('Task deleted');
+    } catch (error) {
+        console.error('Error deleting task:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
